9ea5c35e97d3608613812da0291aa566c798a361
longhotuan <hotuanlong87@gmail.com>
2020-02-09 16:14:25 +0100

    plotting KW

--- lake.R
+++ lake.R
@@ -44,7 +44,7 @@ library(usethis)
 
 temp <- list.files(pattern="*.csv")
 myfiles <- lapply(temp, read_csv)
-colnames_chosen <- colnames(myfiles[[1]])[c(2, 4:6, 13, 14, 16, 18, 19, 20, 31:34, 39:42, 43, 44)]
+colnames_chosen <- colnames(myfiles[[1]])[c(1, 3:5, 12, 13, 15, 17, 18, 19, 33:36, 42:45, 46, 47)]
 
 # choose the useful columns
 myfiles <- lapply(myfiles, subset, select = colnames_chosen)
@@ -55,11 +55,17 @@ factor_convert <- function(x){
     x$`Document Type` <- as.factor(x$`Document Type`)
     x$`Access Type` <- as.factor(x$`Access Type`)
     x$`Abbreviated Source Title` <- as.factor(x$`Abbreviated Source Title`)
-    x <- x %>% filter(Year < 2020)
+    x <- x %>% filter(Year < 2019 & Year > 1955)
 }
 
 myfiles <- lapply(myfiles, factor_convert)
-names(myfiles) <- str_split_fixed(temp, "\\.", 2)[,1]
+
+temp2 <- str_match(string = temp, pattern = "\\_(.*?)\\.")[,2]
+temp3 <- str_match(string = temp, pattern = "(.*?)\\_")[,2]
+temp3 <- paste(temp3, "s", sep = "")
+myfiles <- map2(myfiles, temp2, function(x, y) {x$Country <- y; return(x)})
+myfiles <- map2(myfiles, temp3, function(x, y) {x$Type <- y; return(x)})
+names(myfiles) <- str_match(string = temp, pattern = "(.*?)\\.")[,2]
 
 period_division <- function(x, y, z){x <- x %>% filter(Year < y & Year > z)}
 
@@ -110,73 +116,50 @@ myplot1 <- lapply(myfiles1, KW)
 myplot2 <- lapply(myfiles2, KW)
 myplot3 <- lapply(myfiles3, KW)
 
+
+myplot <- map2(myplot, temp2, function(x, y) {x$Country <- y; return(x)})
+myplot1 <- map2(myplot1, temp2, function(x, y) {x$Country <- y; return(x)})
+myplot2 <- map2(myplot2, temp2, function(x, y) {x$Country <- y; return(x)})
+myplot3 <- map2(myplot3, temp2, function(x, y) {x$Country <- y; return(x)})
+
+myplot <- map2(myplot, temp3, function(x, y) {x$Type <- y; return(x)})
+myplot1 <- map2(myplot1, temp3, function(x, y) {x$Type <- y; return(x)})
+myplot2 <- map2(myplot2, temp3, function(x, y) {x$Type <- y; return(x)})
+myplot3 <- map2(myplot3, temp3, function(x, y) {x$Type <- y; return(x)})
+
 myplot_all <- lapply(myplot, function(x) x <- x[1:40,])
 myplot_all1 <- lapply(myplot1, function(x) x <- x[1:40,])
 myplot_all2 <- lapply(myplot2, function(x) x <- x[1:40,])
 myplot_all3 <- lapply(myplot3, function(x) x <- x[1:40,])
 
-myplot_all <- lapply(names(myplot_all), function(x){
-    if (str_detect(x,"lake")){
-        myplot_all[[x]]$type <- "lake"
-        
-    } else if (str_detect(x,"Reservoir")){
-        myplot_all[[x]]$type <- "reservoir"
-        
-    }; return(myplot_all[[x]])
-})
-myplot_all1 <- lapply(names(myplot_all1), function(x){
-    if (str_detect(x,"lake")){
-        myplot_all1[[x]]$type <- "lake"
-        
-    } else if (str_detect(x,"Reservoir")){
-        myplot_all1[[x]]$type <- "reservoir"
-        
-    }; return(myplot_all1[[x]])
-})
-myplot_all2 <- lapply(names(myplot_all2), function(x){
-    if (str_detect(x,"lake")){
-        myplot_all2[[x]]$type <- "lake"
-        
-    } else if (str_detect(x,"Reservoir")){
-        myplot_all2[[x]]$type <- "reservoir"
-        
-    }; return(myplot_all2[[x]])
-})
-myplot_all3 <- lapply(names(myplot_all3), function(x){
-    if (str_detect(x,"lake")){
-        myplot_all3[[x]]$type <- "lake"
-        
-    } else if (str_detect(x,"Reservoir")){
-        myplot_all3[[x]]$type <- "reservoir"
-        
-    }; return(myplot_all3[[x]])
-})
-
-names(myplot_all) <- names(myplot_all1) <- names(myplot_all2) <-  names(myplot_all3) <-str_split_fixed(temp, "\\.", 2)[,1]
-
-myplot_all_v2 <- map2(myplot_all[1:(length(myplot_all)/2)],myplot_all[(length(myplot_all)/2+1):length(myplot_all)], function(x,y) bind_rows(x,y))
-myplot_all1_v2 <- map2(myplot_all1[1:(length(myplot_all1)/2)],myplot_all1[(length(myplot_all1)/2+1):length(myplot_all1)], function(x,y) bind_rows(x,y))
-myplot_all2_v2 <- map2(myplot_all2[1:(length(myplot_all2)/2)],myplot_all2[(length(myplot_all2)/2+1):length(myplot_all2)], function(x,y) bind_rows(x,y))
-myplot_all3_v2 <- map2(myplot_all3[1:(length(myplot_all3)/2)],myplot_all3[(length(myplot_all3)/2+1):length(myplot_all3)], function(x,y) bind_rows(x,y))
-names(myplot_all_v2) <- names(myplot_all1_v2) <- names(myplot_all2_v2) <- names(myplot_all3_v2) <- c("China", "US")
+myplot_all_v2 <- rbindlist(myplot_all)
+myplot_all1_v2 <- rbindlist(myplot_all1)
+myplot_all2_v2 <- rbindlist(myplot_all2)
+myplot_all3_v2 <- rbindlist(myplot_all3)
 
 # print the graphs
 
 plot_kw <- function(x){
-    ggplot(x, aes(label = keyword, size = n, x = type, color = type)) +
+    ggplot(x, aes(label = keyword, size = n, x = Type, color = Type)) +
         geom_text_wordcloud_area(shape = "diamond") +
-        scale_size_area(max_size = 20) +
+        scale_size_area(max_size = 15) +
         scale_x_discrete(breaks = NULL) +
+        facet_wrap(.~Country, scales = "free") +
         theme_minimal()
 }
 
-myfigure <- lapply(myplot_all_v2, plot_kw)
-myfigure1 <- lapply(myplot_all1_v2, plot_kw)
-myfigure2 <- lapply(myplot_all2_v2, plot_kw)
-myfigure3 <- lapply(myplot_all3_v2, plot_kw)
+myfigure <- split(myplot_all_v2, myplot_all_v2$Country)
+myfigure1 <- split(myplot_all1_v2, myplot_all_v2$Country)
+myfigure2 <- split(myplot_all2_v2, myplot_all_v2$Country)
+myfigure3 <- split(myplot_all3_v2, myplot_all_v2$Country)
+
+myfigure <- lapply(myfigure, plot_kw)
+myfigure1 <- lapply(myfigure1, plot_kw)
+myfigure2 <- lapply(myfigure2, plot_kw)
+myfigure3 <- lapply(myfigure3, plot_kw)
+
 
 lapply(names(myfigure), function(x) ggsave(filename=paste(x,".jpeg",sep=""), plot=myfigure[[x]], units = 'cm', height = 50*1/3, width = 75*1/2, dpi = 300))
 lapply(names(myfigure1), function(x) ggsave(filename=paste(x,"_1.jpeg",sep=""), plot=myfigure1[[x]], units = 'cm', height = 50*1/3, width = 75*1/2, dpi = 300))
 lapply(names(myfigure2), function(x) ggsave(filename=paste(x,"_2.jpeg",sep=""), plot=myfigure2[[x]], units = 'cm', height = 50*1/3, width = 75*1/2, dpi = 300))
 lapply(names(myfigure3), function(x) ggsave(filename=paste(x,"_3.jpeg",sep=""), plot=myfigure3[[x]], units = 'cm', height = 50*1/3, width = 75*1/2, dpi = 300))
-
